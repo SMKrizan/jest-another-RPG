3ef109221f3845d0ccb8c724b5ddcecdfc797528
SMKrizan <smkrizan@gmail.com>
2020-11-02 13:52:11 -0600

    mock potion functioning

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,6 +1,6 @@
 const Potion = require('../lib/Potion');
 
-jest.mock('../lib/Potion.js');
+jest.mock('../lib/Potion');
 
 console.log(new Potion());
 
@@ -13,4 +13,7 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
 })
